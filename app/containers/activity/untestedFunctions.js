7d77996026156793699169d1ab12e10945786c23
Corey Taylor <info@onpub.com>
2015-01-15 09:15:16 -0500

    :sparkles: date_sort_asc

--- app/containers/activity/untestedFunctions.js
+++ app/containers/activity/untestedFunctions.js
@@ -1,3 +1,13 @@
+var date_sort_asc = function (date1, date2) {
+  // This is a comparison function that will result in dates being sorted in
+  // ASCENDING order. As you can see, JavaScript's native comparison operators
+  // can be used to compare dates. This was news to me.
+  if (date1 > date2) return 1;
+  if (date1 < date2) return -1;
+  return 0;
+};
+
+
 updateNotifications(userActVersion) {
   let notifications = {};
   notifications.days = [];
