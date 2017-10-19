db77d6dd7bdba95742e4f2a0f1314f91663fcf56
Erik Ilyin <erik.ilyin@aol.com>
2017-10-19 05:28:13 +0300

    async storage

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,21 +1,19 @@
 
 import { AsyncStorage } from 'react-native';
 import devTools from 'remote-redux-devtools';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import reducer from './reducers';
 import promise from './promise';
 
 export default function configureStore(onCompletion:()=>void):any {
-  // const enhancer = compose(
-  //   applyMiddleware(thunk, promise),
-  //   devTools({
-  //     name: 'nativebasekitchensink', realtime: true,
-  //   }),
-  // );
 
-  const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-  const store = createStoreWithMiddleware(reducer);
+
+  const store = createStore(reducer);
+  const persist = persistStore(store, { storage: AsyncStorage }, onCompletion);
+
+  // const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+  // const store = createStoreWithMiddleware(reducer);
   return store
 }
