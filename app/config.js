e5196a270c62c3445d94d57797982fcb483e0e51
Erik Ilyin <erik.ilyin@aol.com>
2018-09-04 11:30:21 -0400

    removed credentials

--- app/config.js
+++ app/config.js
@@ -1,20 +1,5 @@
 export default {
-    firebase: {
-        apiKey: "AIzaSyBgWbUDbDQjSFn43MhBm3ejdanrbiCVchc",
-        authDomain: "dataapp-3b8c4.firebaseapp.com",
-        databaseURL: "https://dataapp-3b8c4.firebaseio.com",
-        projectId: "dataapp-3b8c4",
-        storageBucket: "dataapp-3b8c4.appspot.com",
-        messagingSenderId: "880222053307"
-    },
     apiHost:'http://mindlogger-girder-atlas.a4vwd5q7ib.us-east-1.elasticbeanstalk.com/api/v1',
-    s3: {
-        bucket: "cmi-dataapp",
-        region: "us-west-1",
-        access_key: "AKIAJSKJBTHX7RPQNMRQ",
-        secret_key: "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",
-        successActionStatus: 201
-    },
     s3_image: {
         bucket: "mindloggerimages"
         
