f07b4b95689b39cf8a214e3bb9a32ae6af8dd7d6
Erik Ilyin <erik.ilyin@aol.com>
2018-09-04 11:14:20 -0400

    api update

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -5,13 +5,13 @@ export default store => next => action => {
     if ((!action.method && !action.path) || action.status) return next(action)
 
     let { path } = action;
-    const { method, type, body, isMultipartUpload } = action;
+    const { method, type, body } = action;
     const state = store.getState();
     let accessToken;
     
     if (state.core && state.core.auth) {
         var auth = state.core.auth;
-        accessToken = auth.access_token;
+        accessToken = auth.token;
     }
     
 
@@ -30,10 +30,9 @@ export default store => next => action => {
     next({...action, status: 'REQUESTED' });
 
     // make the request
-    return makeRequest(method, path, body, accessToken, isMultipartUpload)
+    return makeRequest(method, path, body, accessToken, action)
         .then(response => {
             store.dispatch({...action, response, status: 'COMPLETE' });
-            console.log("API success:",path, body, response)
             return Promise.resolve(response);
         })
         .catch(error => {
@@ -42,36 +41,44 @@ export default store => next => action => {
         });
 }
 
-export const makeRequest = (method, path, body, accessToken, isMultipartUpload) => {
-    const headers = new Headers({
-        "access_token": accessToken,
-    });
-    if (!isMultipartUpload) {
-        headers.set("Content-Type", "application/json; charset=utf-8");
+export const makeRequest = (method, path, data, accessToken, {isMultipartUpload, isJson, extraHeaders}) => {
+    let headers = extraHeaders || {};
+    if (headers['Girder-Authorization']) {
+        
+    } else if (accessToken) {
+        headers["Girder-Token"] = accessToken;
     }
+    
+    let body = data;
+    if (!isMultipartUpload && isJson) {
+        headers.set("Content-Type", "application/json; charset=utf-8");
+        body = JSON.stringify(data);
+    }
+    if (!isJson && method !== 'GET') {
+        body = objectToFormData(data);
+    }
+
     return fetch(`${config.apiHost}${path}`, {
             mode: 'cors',
-            body: isMultipartUpload ? body : JSON.stringify(body),
+            body,
             method,
             headers
         })
         .then(response => {
+            console.log(method, `${config.apiHost}${path}`, body, response);
             const status = response.status;
-            switch (status) {
-                case 401:
-                    
-                    break;
+            try {
+                return response.json().then(json => {
+                    return status === 200 ? Promise.resolve(json) : Promise.reject(json);
+                })
+            } catch (error) {
+                Promise.reject(error);
             }
-            return response.json();
-        })
-        .then(json => {
-            return json.success ? Promise.resolve(json) : Promise.reject(json);
         });
 }
 
 const endpointGenerics = (state, path) => {
     let newPath = path;
-    console.log(state);
     if (state.core.auth && state.core.auth.id) {
         newPath = newPath.replace(/users\/me/, `users/${state.core.auth.id}`);
     }
