0b99b367ffec6e255efc1d7243b82551fce9ae46
Jon Clucas <jon.clucas@childmind.org>
2018-09-18 10:00:53 -0400

    📆  Weekly Notification Days
      Ref #35

--- app/containers/activity/untestedFunctions.js
+++ app/containers/activity/untestedFunctions.js
@@ -2,7 +2,7 @@ updateNotifications(userActVersion) {
   let notifications = {};
   notifications.days = [];
   notifications.times = [];
-  let today = new Date();
+  let today = new Date(); // Now.
   if (userActVersion.meta.userNotifications.scheduleType.calendar && userActVersion.meta.userNotifications.scheduleType.calendar.length) {
 
   }
@@ -10,14 +10,20 @@ updateNotifications(userActVersion) {
 
   }
   if (userActVersion.meta.userNotifications.scheduleType.weekly && userActVersion.meta.userNotifications.scheduleType.weekly.length) {
-    todayDay = today.getDay();
-    jsDays = {"Sun": 0, "Mon": 1, "Tues": 2, "Wed": 3, "Thu": 4, "Fri": 5, "Sat": 6};
-    userActVersion.meta.userNotifications.scheduleType.weekly.forEach(day) {
+    todayDay = today.getDay(); // Today's day of the week.
+    jsDays = {"Sun": 0, "Mon": 1, "Tue": 2, "Wed": 3, "Thu": 4, "Fri": 5, "Sat": 6};
+    userActVersion.meta.userNotifications.scheduleType.weekly.forEach(function(day) {
       if (day.length) {
-        let dateAssign = new Date(today);
-        dateAssign.setTime(dateAssign.getTime() + (jsDays[day] - todayDay + (jsDays[day] <= todayDay ? 7 : 0)) * 86400000);
-        notifications.days.push(dateAssign);
+        let aftermorrows = [jsDays[day] - todayDay + (jsDays[day] <= todayDay ? 7 : 0)]; // Get the appropriate weekday this week unless that day is today or earlier, in which case get next week.
+        for (var plusDays=0; aftermorrows[plusDays]<=28; plusDays++) { // Calculate 5 weeks of days.
+          aftermorrows.push(aftermorrows[plusDays]+7); // The following x-day.
+        }
+        aftermorrows.forEach(function(afterday){
+          let dateAssign = new Date(today); // Date object to mutate.
+          dateAssign.setTime(dateAssign.getTime() + afterday * 86400000); // Calculate date of future assignments
+          notifications.days.push(dateAssign); // Push assignment date to Array.
+        });
       }
-    }
+    });
   }
 }
