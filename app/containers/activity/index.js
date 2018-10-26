76191d82c0db372ae8ecccd712ac35911c18053b
Erik Ilyin <erik.ilyin@aol.com>
2018-10-25 22:11:03 -0400

    push notification configure

--- app/containers/activity/index.js
+++ app/containers/activity/index.js
@@ -58,8 +58,50 @@ class ActivityScreen extends Component {
         } else {
             this.scheduleNotifications(acts);
         }
+        PushNotification.configure({
+
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: function(token) {
+                console.log( 'TOKEN:', token );
+            },
+        
+            // (required) Called when a remote or local notification is opened or received
+            onNotification: function(notification) {
+                console.log( 'NOTIFICATION:', notification );
+        
+                // process the notification
+                if (Platform.OS == 'ios') {
+                    this.onNotificationIOS(notification);
+                } else {
+                    this.onNotificationAndroid(notification);
+                }
+                // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+        
+            // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
+            //senderID: "YOUR GCM (OR FCM) SENDER ID",
+        
+            // IOS ONLY (optional): default: all - Permissions to register.
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
+        
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
+        
+            /**
+              * (optional) default: true
+              * - Specified if permissions (ios) and token (android and ios) will requested or not,
+              * - if not, you must call PushNotificationsHandler.requestPermissions() later
+              */
+            requestPermissions: true,
+        });
         if (Platform.OS == 'ios') {
-            PushNotificationIOS.addEventListener('localNotification',this.onNotificationIOS);
+            //PushNotificationIOS.addEventListener('localNotification',this.onNotificationIOS);
         } else {
             PushNotification.registerNotificationActions(['Take', 'Cancel'])
         }
@@ -226,14 +268,14 @@ class ActivityScreen extends Component {
     }
 
     onNotificationIOS = (notification) => {
-        let {userInfo} = notification;
+        let {data:{userInfo}} = notification;
         if(userInfo)
             this.startActivityFromNotification(userInfo.actId)
     }
 
     onNotificationAndroid = (notification) => {
         const { acts } = this.props;
-        let index = parseInt(notification.id);
+        let index = parseInt(notification.data.id);
         if(acts[index])
             this.startActivityFromNotification(acts[index]._id);
     }
