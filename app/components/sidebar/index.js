25d4429713adce4db34518e2baba26cc233b8d90
Erik Ilyin <erik.ilyin@aol.com>
2018-12-03 09:44:01 +0800

    cancel all notification on logout

--- app/components/sidebar/index.js
+++ app/components/sidebar/index.js
@@ -5,6 +5,7 @@ import { Image } from 'react-native';
 import { connect } from 'react-redux';
 import { Content, Text, List, ListItem, Container, Left, Right, Badge, View } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import PushNotification from 'react-native-push-notification';
 
 import { changePlatform, changeMaterial, closeDrawer } from '../../actions/drawer';
 import styles from './style';
@@ -66,6 +67,7 @@ class SideBar extends Component {
   onMenu(route) {
     const {closeDrawer, signOut} = this.props;
     if(route == 'logout') {
+      PushNotification.cancelAllLocalNotifications();
       signOut();
       Actions.reset('login');
     } else {
