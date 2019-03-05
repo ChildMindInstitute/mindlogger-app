2885a9e8de22080ea1b6ac91220244cf68c0a6f7
Stu Freen <stuart.freen@rangle.io>
2019-03-05 17:25:48 -0500

    Refactor SurveyTableSelector

--- app/widgets/survey/SurveyTableSelector.js
+++ app/widgets/survey/SurveyTableSelector.js
@@ -1,127 +1,86 @@
-import React, {Component} from 'react';
-import {StyleSheet, TouchableOpacity, ImageBackground, Image} from 'react-native';
-import { Content, List, ListItem, Text, Button, Right, Body, Item, Input, Row, Col, Radio, CheckBox, H2, View, Grid, Thumbnail } from 'native-base';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, View } from 'native-base';
+import SurveyTableSelectorCell from './SurveyTableSelectorCell';
 
-import baseTheme from '../../theme';
-import styles from './styles';
-import GImage from '../../components/image/Image';
-
-class SurveyTableSelector extends Component {
-    constructor(props) {
-        super(props);
+export default class SurveyTableSelector extends Component {
+  constructor(props) {
+    super(props);
+    const { config, answer, onChange } = props;
+    const { rows, cols } = config;
+    // Initialize answer to zeros or empty strings if nothing set
+    if (typeof answer === 'undefined') {
+      const zeroesAr = config.mode === 'select'
+        ? rows.map(() => [])
+        : rows.map(() => cols.map(() => false));
+      onChange(zeroesAr);
     }
+  }
 
-    componentWillMount() {
-        this.updateAnswer(this.props);
+  onChoiceSelect = (rowIdx, colIdx) => {
+    const { answer, config: { optionsMin, optionsMax }, onChange } = this.props;
+    const index = answer[rowIdx].indexOf(colIdx);
+    if (index < 0) {
+      answer[rowIdx].push(colIdx);
+    } else {
+      answer[rowIdx].splice(index, 1);
     }
-
-    updateAnswer({answer, config}) {
-        answer = answer || [];
-        const {rows, mode} = config;
-        if(answer.length<rows.length) {
-            switch(config.mode) {
-                case 'select':
-                    answer = rows.map((row) => [])
-                    break;
-                case 'order':
-                    answer = rows.map((row)=>cols.map( (col) => false ))
-                    break;
-            }
-        }
-        this.setState({answer})
-    }
-
-    componentWillReceiveProps = (nextProps) => {
-        if (nextProps.answer != this.props.answer) {
-            this.updateAnswer(nextProps);
-        }
-    }
-
-    onChoiceSelect(rowIdx, colIdx) {
-        let {answer} = this.state;
-        const {config:{optionsMin, optionsMax, rows}, onChange} = this.props;
-        const index = answer[rowIdx].indexOf(colIdx);
-        if (index<0) {
-            answer[rowIdx].push(colIdx);
-        } else {
-            answer[rowIdx].splice(index, 1);
-        }
-        this.setState({answer});
-        let validate = true;
-        answer.forEach(e => {
-            if (e.length<optionsMin || e.length>optionsMax)
-                validate = false;
-        });
-        onChange(answer, validate);
-    }
-
-    renderCell(config, rowIdx, colIdx) {
-        const {answer} = this.state
-        const cell = config.rows[rowIdx][colIdx];
-        switch(cell.type) {
-            case 'text':
-                return (<Button 
-                    style={styles.buttonStyle}
-                    transparent={!answer[rowIdx].includes(colIdx)}
-                    onPress={() => this.onChoiceSelect(rowIdx, colIdx) }>
-                    <Text>{cell.text}</Text>
-                    </Button>)
-            case 'file':
-                return (<TouchableOpacity key={colIdx} onPress={() => {
-                    this.onChoiceSelect(rowIdx, colIdx)
-                }}>
-                <GImage style={answer[rowIdx].includes(colIdx) ? { ...this.imageStyle, borderWidth: 3, borderColor: '#ee5555'} : this.imageStyle} file={config.rows[rowIdx][colIdx].file} />
-                </TouchableOpacity>)
-            case 'multi_sel':
-                return (<TouchableOpacity style={styles.buttonStyle} transparent onPress={() => this.onMultiSelect(rowIdx, colIdx) }><CheckBox style={{marginLeft:-4}} checked={answer[rowIdx][colIdx]} onPress={() => this.onMultiSelect(rowIdx, colIdx) } /></TouchableOpacity>)
-            
-            default:
-                  return (<Text></Text>)
+    let validate = true;
+    answer.forEach((e) => {
+      if (e.length < optionsMin || e.length > optionsMax) {
+        validate = false;
       }
-    }
-    render() {
-        const { answer, config} = this.props;
-        let height = 60
-        if(this.state.dimensions && config.mode == 'select') {
-            height = this.state.dimensions.width/(config.colsCount + 1)
-        }
-        const cellStyle = {
-            height,
-            padding: 4,
-            alignItems: 'center',
-            alignContent: 'center',
-            justifyContent: 'center',
-        }
-        const rowStyle = {
-            height
-        }
-        this.imageStyle = {
-            width: (height-3),
-            height: (height-3),
-            padding: 3,
-        }
-        return (
-            <View onLayout={this.onLayout}>
-                {config.rows.map((cols, rowIdx) => (
-                    <Row style={rowStyle} key={rowIdx}>
-                        {cols.map( (cell, colIdx) => <Col key={colIdx} style={cellStyle}>{this.renderCell(config, rowIdx, colIdx)}</Col> )}
-                    </Row>)
-                )}
-            </View>
-        )
-    }
+    });
+    onChange(answer, validate);
+  }
 
-    onLayout = event => {
-        if (this.state.dimensions) return // layout was already called
-        let {width, height, top, left} = event.nativeEvent.layout
-        this.setState({dimensions: {width, height, top, left}})
-    }
+  render() {
+    const { answer, config } = this.props;
+    const height = 60;
+    // if(this.state.dimensions && config.mode == 'select') {
+    //   height = this.state.dimensions.width/(config.colsCount + 1)
+    // }
+
+    const cellStyle = {
+      height,
+      padding: 4,
+      alignItems: 'center',
+      alignContent: 'center',
+      justifyContent: 'center',
+    };
+    const rowStyle = {
+      height,
+    };
+    return (
+      <View onLayout={this.onLayout}>
+        {config.rows.map((cols, rowIdx) => (
+          <Row style={rowStyle} key={rowIdx}>
+            {cols.map((cell, colIdx) => (
+              <Col key={colIdx} style={cellStyle}>
+                <SurveyTableSelectorCell
+                  cell={config.rows[rowIdx][colIdx]}
+                  isSelected={answer[rowIdx].includes(colIdx)}
+                  onPress={() => {
+                    this.onChoiceSelect(rowIdx, colIdx);
+                  }}
+                  height={height}
+                />
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </View>
+    );
+  }
 }
 
-export default connect(state => ({
-    
-  }),
-  (dispatch) => ({
-  })
-)(SurveyTableSelector);
+SurveyTableSelector.propTypes = {
+  config: PropTypes.shape({
+    rows: PropTypes.arrayOf(PropTypes.string),
+    cols: PropTypes.arrayOf(PropTypes.string),
+    optionsMin: PropTypes.number,
+    optionsMax: PropTypes.number,
+  }).isRequired,
+  answer: PropTypes.arrayOf(PropTypes.array).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
---
+++ app/widgets/survey/SurveyTableSelectorCell.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity } from 'react-native';
+import { Text, Button, GImage } from 'native-base';
+import styles from './styles';
+
+const SurveyTableSelectorCell = ({ cell, isSelected, onPress, height = 60 }) => {
+  const imageStyle = {
+    width: (height - 3),
+    height: (height - 3),
+    padding: 3,
+  };
+  switch (cell.type) {
+    case 'text':
+      return (
+        <Button
+          style={styles.buttonStyle}
+          transparent={!isSelected}
+          onPress={onPress}
+        >
+          <Text>{cell.text}</Text>
+        </Button>
+      );
+    case 'file':
+      return (
+        <TouchableOpacity onPress={onPress}>
+          <GImage
+            style={imageStyle}
+            file={cell.file}
+          />
+        </TouchableOpacity>
+      );
+    default:
+      return (<Text />);
+  }
+};
+
+SurveyTableSelectorCell.propTypes = {
+  cell: PropTypes.shape({
+    file: PropTypes.number,
+    text: PropTypes.string,
+    type: PropTypes.string,
+  }).isRequired,
+  onPress: PropTypes.func.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  height: PropTypes.number.isRequired,
+};
+
+export default SurveyTableSelectorCell;
