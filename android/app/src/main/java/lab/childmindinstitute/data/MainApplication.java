66625397fcd8d28e6182fc0590bef41a2427c645
henryrossiter <rossiterhenry@gmail.com>
2019-03-11 20:05:08 -0500

    uno mas

--- android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
+++ android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
@@ -35,7 +35,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
           new RNDeviceInfo(),
           new RNSensorsPackage(),
           new ImagePickerPackage(),
