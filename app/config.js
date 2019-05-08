9fe417117d55e3ffdef743e766bca98f01c49891
Henry Rossiter <henry.rossiter@utexas.edu>
2019-05-08 13:02:30 -0500

    linting:

--- app/config.js
+++ app/config.js
@@ -1,13 +1,12 @@
 export default {
-    defaultApiHost:'https://api.mindlogger.info/api/v1',
-    defaultSkin : { "name" : "MindLogger",
-                    "colors" : {
-                        "primary" : '#0067A0',
-                        "secondary" : '#FFFFFF',
-                      },
-                  },
-    s3_image: {
-        bucket: "mindloggerimages"
-
-    }
+  defaultApiHost: 'https://api.mindlogger.info/api/v1',
+  defaultSkin: { name: 'MindLogger',
+    colors: {
+      primary: '#0067A0',
+      secondary: '#FFFFFF',
+    },
+  },
+  s3_image: {
+    bucket: 'mindloggerimages'
+  }
 }
