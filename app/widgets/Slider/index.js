c1c9367b9181da1e58182f77b3e7631479143fb1
akeshavan <keshavan@berkeley.edu>
2019-05-16 18:25:26 -0700

    enh: slider now has smileys

--- app/widgets/Slider/index.js
+++ app/widgets/Slider/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import { Text } from 'native-base';
+import { CachedImage } from 'react-native-img-cache';
 import SliderComponent from './slider';
 
+/**
+ * getURL will replace an SVG image with a JPG image because
+ * react-native can't handle SVGs, but web prefers them.
+ * @param {String} url 
+ */
+const getURL = (url) => {
+  if (url.endsWith('.svg')) {
+    return url.replace('.svg', '.jpg');
+  }
+  return url;
+};
+
 export const Slider = ({ config: { maxValue, minValue, itemList }, value, onChange }) => (
-  <View style={{ alignItems: 'stretch', minHeight: 400 }}>
+  <View style={{ alignItems: 'stretch', minHeight: 450 }}>
     <Text style={{ textAlign: 'center' }}>{maxValue}</Text>
+    { itemList[itemList.length-1].image ? <View style={{justifyContent: 'center', alignItems: 'center'}}><CachedImage style={{ width: 45, height: 45, resizeMode: 'cover' }} source={{ uri: getURL(itemList[itemList.length-1].image.en) }} /></View> : <View></View>}
     <SliderComponent
       value={value || 0}
       min={1}
@@ -17,6 +31,7 @@ export const Slider = ({ config: { maxValue, minValue, itemList }, value, onChan
       selected={!!value}
       onChange={onChange}
     />
+    { itemList[0].image ? <View style={{justifyContent: 'center', alignItems: 'center'}}><CachedImage style={{ width: 45, height: 45, resizeMode: 'cover' }} source={{ uri: getURL(itemList[0].image.en) }} /></View> : <View></View>}
     <Text style={{ textAlign: 'center' }}>{minValue}</Text>
   </View>
 );
