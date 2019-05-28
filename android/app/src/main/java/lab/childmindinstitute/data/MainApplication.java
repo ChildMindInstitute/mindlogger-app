e792b22e7cafd9e9d3cb0119f23af33c9f0932d3
Stu Freen <stuart.freen@rangle.io>
2019-05-28 12:31:06 -0400

    Link react-native-netinfo package

--- android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
+++ android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
@@ -3,6 +3,7 @@ package lab.childmindinstitute.data;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new NetInfoPackage(),
           new RNCWebViewPackage(),
           new RNCameraPackage(),
           new RNDeviceInfo(),
