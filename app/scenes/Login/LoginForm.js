ed2e8c808508c1ea286563793bd86706337907c5
Henry Rossiter <henry.rossiter@utexas.edu>
2019-05-28 10:18:52 -0500

    netinfo hook, network alert if disconnected on login (#195)

--- app/scenes/Login/LoginForm.js
+++ app/scenes/Login/LoginForm.js
@@ -6,40 +6,49 @@ import {
   Form,
 } from 'native-base';
 import { reduxForm, Field, propTypes } from 'redux-form';
+import { useNetInfo } from '@react-native-community/netinfo';
 import { colors } from '../../theme';
 import { FormInputItem, required } from '../../components/form/FormItem';
 import styles from './styles';
 
-const LoginForm = ({ handleSubmit, submitting, error, primaryColor }) => (
-  <Form>
-    <Field
-      component={FormInputItem}
-      placeholder="Username"
-      placeholderTextColor={colors.secondary_50}
-      name="user"
-      autoCapitalize="none"
-      style={styles.text}
-      validate={required}
-      errorStyle={{ color: colors.secondary }}
-    />
-    <Field
-      component={FormInputItem}
-      placeholder="Password"
-      placeholderTextColor={colors.secondary_50}
-      name="password"
-      autoCapitalize="none"
-      style={styles.text}
-      secureTextEntry
-      validate={required}
-      errorStyle={{ color: colors.secondary }}
-    />
-    <Button style={styles.button} block onPress={handleSubmit} disabled={submitting}>
-      {submitting
-        ? <ActivityIndicator color={primaryColor} />
-        : <Text style={[styles.buttonText, { color: primaryColor }]}>LOGIN</Text>}
-    </Button>
-  </Form>
-);
+const LoginForm = ({ handleSubmit, submitting, primaryColor, connectionAlert }) => {
+  const netInfo = useNetInfo();
+  return (
+    <Form>
+      <Field
+        component={FormInputItem}
+        placeholder="Username"
+        placeholderTextColor={colors.secondary_50}
+        name="user"
+        autoCapitalize="none"
+        style={styles.text}
+        validate={required}
+        errorStyle={{ color: colors.secondary }}
+      />
+      <Field
+        component={FormInputItem}
+        placeholder="Password"
+        placeholderTextColor={colors.secondary_50}
+        name="password"
+        autoCapitalize="none"
+        style={styles.text}
+        secureTextEntry
+        validate={required}
+        errorStyle={{ color: colors.secondary }}
+      />
+      <Button
+        style={styles.button}
+        block
+        onPress={(netInfo.isConnected) ? handleSubmit : connectionAlert}
+        disabled={submitting}
+      >
+        {submitting
+          ? <ActivityIndicator color={primaryColor} />
+          : <Text style={[styles.buttonText, { color: primaryColor }]}>LOGIN</Text>}
+      </Button>
+    </Form>
+  );
+};
 
 LoginForm.propTypes = {
   ...propTypes,
