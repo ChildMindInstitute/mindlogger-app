6f898ca36562a220704e48dd66669db624b9d544
Jon Clucas <jon.clucas@childmind.org>
2019-06-27 13:55:49 -0400

    :rocket: :robot: :green_heart: Google Play Deployment in BitRise (#228)
    * :rocket: mindlogger.org
    
    - api.mindlogger.info → api.mindlogger.org
    
    Ref https://github.com/ChildMindInstitute/mindlogger-website/issues/8#issuecomment-501420867
    
    * :books: v0.1 Deprecated
    
    - :green_heart: :robot: APK Path
    - :bookmark: v0.2.10(2)
    
    * :construction: 64 bit Support
    
    * :construction:
    
    Ref #222
    
    * :robot: :arrow_up: Add 64-bit Support for Android
    
    Ref #215
    Resolves #222
    
    * :green_heart: :robot: :rocket: :closed_lock_with_key: Play Store Signing :bookmark: v0.2.12(6)
    
    Ref #215
    
    * :green_heart: :robot: "I’d suggest you to remove the `signingConfigs` section from there"
    
    Ref https://discuss.bitrise.io/t/cant-create-a-release-apk-with-keystore-file/4086/4, #215
    
    * fixup! :rocket: :robot: :apple: Build, Sign, and Deploy to iOS and Android in Sync
    
    * :green_heart: Always Increment Version Code
    
    * :robot: :ambulance: React Native vs AAB
    
    - `android.bundle.enableUncompressedNativeLibs=false`
    
    Ref
    https://github.com/ChildMindInstitute/mindlogger-app/issues/215#issuecomment-504218419,
    https://issuetracker.google.com/issues/122953695#comment5
    
    * Build failed with an exception react native
    
    Ref https://stackoverflow.com/a/56648987
    
    * :green_heart: :bookmark: v0.3.2
    
    * :bookmark: v0.3.3
    
    * :green_heart: :ambulance: Require Slider Package :bookmark: v0.3.6(36)
    
    Ref #215, https://github.com/ChildMindInstitute/mindlogger-app/issues/215#issuecomment-504218419, #228, #232
    
    * :green_heart: :robot: :rocket: Google 16.1.0+ :bookmark: v0.3.7
    
    * :construction:
    
    * :green_heart: :rocket: :robot: Fix Android Build Flow :bookmark: v0.3.8(40)
    
    Resolves #215, #228
    
    Ref https://github.com/facebook/react-native/issues/25294
    
    * :green_heart: :pencil: react-native-push-notifications⃠
    
    * :green_heart: :robot: :rocket: Jitpack → Google
    
    * :green_heart: :robot: :rocket: Specify exifinterface Version
    
    - :shirt:
    
    * :bookmark: v0.3.9(43)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,19 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [0.3.4‒0.3.6] - 2019-06-25‒26
+- Update slider bar (horizontal only)
+
+## [0.3.1‒0.3.3; 0.3.7‒0.3.9] - 2019-06-20; 2019-06-25‒27
+- Bug fix for Android build
+
+## [0.3.0] - 2019-06-14
+- Android and iOS versions now locked in sync.
+
+## [0.2.12] - 2019-06-14
+### Added
+- :robot: :arrow_up: 64-bit support for Android
+
 ## [0.2.11] - 2019-06-13
 ### Changed
 - Offline media loading behavior
--- android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
+++ android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
@@ -3,6 +3,8 @@ package lab.childmindinstitute.data;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -34,6 +36,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new VectorIconsPackage(),
+          new ReactSliderPackage(),
           new NetInfoPackage(),
           new RNCWebViewPackage(),
           new RNCameraPackage(),
