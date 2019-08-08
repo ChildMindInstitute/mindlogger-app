75ba65adc54016423ea2341092bfbeaab82db099
akeshavan <anishakeshavan@gmail.com>
2019-08-08 12:52:19 -0700

    fun fun colors

--- app/components/core/TouchBox.js
+++ app/components/core/TouchBox.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { colors } from '../../theme';
 
 const styles = StyleSheet.create({
   box: {
-    borderRadius: 3,
+    borderRadius: 12,
     elevation: 3,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    backgroundColor: '#F0F0F0',
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 1 },
+    // shadowOpacity: 0.3,
+    // shadowRadius: 2,
+    borderStyle: 'solid',
+    borderColor: colors.lightGrey,
+    borderWidth: 4,
+    backgroundColor: 'white', // '#F0F0F0',
     padding: 16,
     margin: 20,
     marginTop: 10,
