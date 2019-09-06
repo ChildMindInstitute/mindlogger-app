307b0f25526ac981056ac637cceb20f8ee3559d7
akeshavan <anishakeshavan@gmail.com>
2019-09-06 12:01:02 -0700

    enh: added a Timeline Chart using D3

--- app/components/AppletData/ItemChart.js
+++ app/components/AppletData/ItemChart.js
@@ -4,6 +4,7 @@ import { StyleSheet, StatusBar, Text, View } from 'react-native';
 
 import { VictoryBar, VictoryChart, VictoryLabel } from 'victory-native';
 import { colors } from '../../themes/colors';
+import TimelineChart from './TimelineChart';
 
 const data = [
   { x: '?', y: 1 },
@@ -16,12 +17,41 @@ class ItemChart extends React.Component {
 
   // eslint-disable-next-line
   renderTimelinePlot() {
+    const { item } = this.props;
 
+    const labels = item.valueConstraints.itemList.map(i => ({ name: i.name.en, value: i.value }));
+    const data = [
+      {
+        value: 0,
+        date: '2019-09-06',
+      },
+      {
+        value: 0,
+        date: '2019-09-05',
+      },
+      {
+        value: 1,
+        date: '2019-09-04',
+      },
+      {
+        value: 1,
+        date: '2019-09-03',
+      },
+      {
+        value: 0,
+        date: '2019-09-01',
+      },
+    ];
+    return (
+      <TimelineChart data={data} labels={labels} />
+    );
   }
 
   // eslint-disable-next-line
   renderLinePlot() {
-
+    return (
+      <Text>Line Plot</Text>
+    );
   }
 
   // eslint-disable-next-line
@@ -45,14 +75,34 @@ class ItemChart extends React.Component {
     );
   }
 
+  // eslint-disable-next-line
+  renderChartByItemType() {
+    const { item } = this.props;
+    switch (item.inputType) {
+      case 'radio':
+        return this.renderTimelinePlot();
+      case 'slider':
+        return this.renderLinePlot();
+      case 'timeRange':
+        return this.renderBarPlot();
+      default:
+        return (
+          <Text>
+            No chart type specified.
+          </Text>
+        );
+    }
+  }
+
   render() {
     const { item } = this.props;
+    // console.log('item is', item);
     return (
       <View style={{ alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontWeight: 'bold' }}>
+        <Text style={{ fontWeight: 'bold', paddingBottom: 20 }}>
           { item.description ? item.description.en : '' }
         </Text>
-
+        {this.renderChartByItemType()}
       </View>
     );
   }
---
+++ app/components/AppletData/TimelineChart.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dimensions } from 'react-native';
+import moment from 'moment';
+import {
+  Svg,
+  Line,
+  Text,
+  Circle,
+} from 'react-native-svg';
+import { min, max } from 'd3-array';
+import { scaleTime } from 'd3-scale';
+
+import { colors } from '../../themes/colors';
+
+// eslint-disable-next-line
+class TimelineChart extends React.Component {
+
+  // eslint-disable-next-line
+  renderSingle(label, data, xTicks, xMapper) {
+    const width = Math.round(Dimensions.get('window').width * 0.9);
+    return (
+      <Svg width={width} height={50} key={label.name}>
+        <Text x="0" y="10">{label.name}</Text>
+        <Line x1="0" y1="25" x2={width} y2="25" stroke={colors.lightGrey} strokeWidth="2" />
+        {
+          xTicks.map(x => <Circle x={x} y="25" r="5" fill={colors.lightGrey} />)
+        }
+        {
+          // only plot data where it matches label's value.
+          data.filter(x => x.value === label.value).map(x => <Circle x={xMapper(moment(x.date).toDate())} y="25" r="5" fill={colors.primary} />)
+        }
+      </Svg>
+    );
+  }
+
+  render() {
+    const { data, labels } = this.props;
+    const width = Math.round(Dimensions.get('window').width * 0.9);
+    // 1. calculate minimum and maximum date
+    const dateArray = data.map(d => moment(d.date).toDate());
+    let minDate = min(dateArray);
+    const minDateMoment = moment(minDate);
+    const maxDate = max(dateArray);
+    const maxDateMoment = moment(maxDate);
+
+    // 2. min date should at least be 7 days before max date
+    let diffDays = moment(maxDateMoment).diff(minDateMoment, 'days');
+    diffDays = max([diffDays, 7]);
+    minDate = maxDateMoment.subtract(diffDays, 'days').toDate();
+
+    // 3. create linear mapping between width and min,max
+    const xMapper = scaleTime().domain([minDate, maxDate]).range([5, width - 5]).nice();
+
+    // 4. calculate steps by day, between min and max date
+
+    const dateTicks = [];
+    for (let i = 0; i < diffDays + 1; i += 1) {
+      dateTicks.push(moment(minDate).add(i, 'days').toDate());
+    }
+
+    // 5. put through mapper
+    const xTicks = dateTicks.map(xMapper);
+
+    return labels.map(label => this.renderSingle(label, data, xTicks, xMapper));
+  }
+}
+
+TimelineChart.propTypes = {
+  data: PropTypes.array.isRequired,
+  labels: PropTypes.array.isRequired,
+};
+
+export default TimelineChart;
