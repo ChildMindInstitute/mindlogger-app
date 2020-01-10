b7237ebc144db25c18634768fd482645192a0136
Benjamin Thonet <devbtech728@gmail.com>
2020-01-10 18:23:39 +0100

    each tab(except all) should have a red dot in indicator if it contains overdue activies

---
+++ app/scenes/AppletTabs/TabItem.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Svg, { Defs, LinearGradient, Stop, Circle } from 'react-native-svg';
+import theme from '../../themes/variables';
+import { colors } from '../../themes/colors';
+import AppletImage from '../../components/AppletImage';
+import { NotificationText } from '../../components/core/NotificationText';
+import AllApplets, { ALL_APPLETS_ID } from '../../components/AllApplets';
+
+const size = 64;
+const fontSize = 12;
+const scaleFactor = 0.93;
+const padding = 5;
+
+const styles = StyleSheet.create({
+  tab: {
+    padding,
+    overflow: 'hidden',
+    width: size + 2 * padding,
+    height: size + 3 * padding + fontSize,
+  },
+
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  tabImage: {
+    overflow: 'hidden',
+    width: scaleFactor * size,
+    height: scaleFactor * size,
+    borderRadius: scaleFactor * size / 2,
+  },
+
+  tabTitle: {
+    fontFamily: theme.fontFamily,
+    fontWeight: 'bold',
+    fontSize,
+    marginBottom: 5,
+    color: colors.tertiary,
+    marginTop: padding,
+    alignSelf: 'center',
+  },
+  notification: {
+    position: 'absolute',
+    top: 0,
+    right: 10,
+  },
+});
+
+const TabItem = ({ applet, onPress, selected }) => {
+  const numberOverdue = applet.id === ALL_APPLETS_ID ? 0 : applet.activities.reduce(
+    (accumulator, activity) => (activity.isOverdue ? accumulator + 1 : accumulator),
+    0,
+  );
+  // if (applet.name.en === 'Cognitive Tasks') {
+  //   numberOverdue = 1;
+  // }
+  const renderBorder = (
+    <Svg height={size} width={size} style={{ position: 'absolute' }}>
+      <Defs>
+        <LinearGradient id="grad" x1={size} y1={0} x2={0} y2={size}>
+          <Stop offset="0" stopColor="#902F89" stopOpacity="1" />
+          <Stop offset="1" stopColor="#EFAF5A" stopOpacity="1" />
+        </LinearGradient>
+      </Defs>
+      <Circle cx={size / 2} cy={size / 2} r={size / 2} fill="url(#grad)" />
+    </Svg>
+  );
+
+  return (
+    <React.Fragment>
+      <TouchableOpacity onPress={() => onPress(applet)} key={applet.id}>
+        <View style={[
+          styles.tab,
+          selected && { backgroundColor: colors.lightBlue },
+        ]}
+        >
+          <View style={styles.center}>
+            {renderBorder}
+            <View style={styles.tabImage}>
+              {applet.id === ALL_APPLETS_ID
+                ? <AllApplets.AllAppletsIcon size={scaleFactor * size} />
+                : <AppletImage applet={applet} size={scaleFactor * size} />}
+            </View>
+          </View>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={styles.tabTitle}
+          >{applet.name.en}
+          </Text>
+          {numberOverdue > 0 && (
+            <View style={styles.notification}>
+              <NotificationText>{numberOverdue}</NotificationText>
+            </View>
+          )}
+        </View>
+      </TouchableOpacity>
+    </React.Fragment>
+  );
+};
+
+TabItem.propTypes = {
+  applet: PropTypes.object.isRequired,
+  selected: PropTypes.bool,
+  onPress: PropTypes.func.isRequired,
+};
+
+TabItem.defaultProps = {
+  selected: false,
+};
+
+export default TabItem;
--- app/scenes/AppletTabs/TabsView.js
+++ app/scenes/AppletTabs/TabsView.js
@@ -1,87 +1,39 @@
 import React from 'react';
-import { ScrollView, View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
-import Svg, { Defs, LinearGradient, Stop, Circle } from 'react-native-svg';
-import AppletImage from '../../components/AppletImage';
-import AllApplets, { ALL_APPLETS_ID } from '../../components/AllApplets';
-import theme from '../../themes/variables';
-import { colors } from '../../themes/colors';
+import TabItem from './TabItem';
+import { ALL_APPLETS_ID } from '../../components/AllApplets';
 import { sortAppletsAlphabetically } from '../../services/helper';
-
-const size = 64;
-const fontSize = 12;
-const scaleFactor = 0.93;
-const padding = 5;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
   },
-
-  tab: {
-    padding,
-    overflow: 'hidden',
-    width: size + 2 * padding,
-    height: size + 3 * padding + fontSize,
-  },
-
-  center: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  tabImage: {
-    overflow: 'hidden',
-    width: scaleFactor * size,
-    height: scaleFactor * size,
-    borderRadius: scaleFactor * size / 2,
-  },
-
-  tabTitle: {
-    fontFamily: theme.fontFamily,
-    fontWeight: 'bold',
-    fontSize,
-    marginBottom: 5,
-    color: colors.tertiary,
-    marginTop: padding,
-    alignSelf: 'center',
-  },
 });
 
-// eslint-disable-next-line
 class TabsView extends React.Component {
-  fireOnInitial: false;
-
   onPress = (applet) => {
     this.props.onPress(applet);
     this.fireOnInitial = true;
-  }
+  };
 
-  renderTabs(applets, currentApplet, renderBorder) {
+  renderTabs(applets, currentApplet) {
     return (
       <React.Fragment>
         {applets.map(applet => (
-          <TouchableOpacity onPress={() => this.onPress(applet)} key={applet.id}>
-            <View style={[
-              styles.tab,
-              this.fireOnInitial
-                  && currentApplet.id === applet.id
-                  && { backgroundColor: colors.lightBlue },
-            ]}
-            >
-              <View style={styles.center}>
-                {renderBorder}
-                <View style={styles.tabImage}>
-                  <AppletImage applet={applet} size={scaleFactor * size} />
-                </View>
-              </View>
-              <Text numberOfLines={1} ellipsizeMode="tail" style={styles.tabTitle}>{applet.name.en}</Text>
-            </View>
-          </TouchableOpacity>
+          <TabItem
+            applet={applet}
+            onPress={() => this.onPress(applet)}
+            key={applet.id}
+            selected={this.fireOnInitial
+            && currentApplet.id === applet.id}
+          />
+
         ))}
       </React.Fragment>
     );
   }
+
 
   render() {
     const { applets, onPress, currentApplet, sortedAlphabetically } = this.props;
@@ -89,45 +41,27 @@ class TabsView extends React.Component {
     if (sortedAlphabetically) {
       sortAppletsAlphabetically(data);
     }
-    const renderBorder = (
-      <Svg height={size} width={size} style={{ position: 'absolute' }}>
-        <Defs>
-          <LinearGradient id="grad" x1={size} y1={0} x2={0} y2={size}>
-            <Stop offset="0" stopColor="#902F89" stopOpacity="1" />
-            <Stop offset="1" stopColor="#EFAF5A" stopOpacity="1" />
-          </LinearGradient>
-        </Defs>
-        <Circle cx={size / 2} cy={size / 2} r={size / 2} fill="url(#grad)" />
-      </Svg>
-    );
+    const allAppletsItem = {
+      id: ALL_APPLETS_ID,
+      name: { en: 'All applets' },
+    };
+
     return (
       <React.Fragment>
         <ScrollView
           horizontal
           style={styles.container}
         >
-          <TouchableOpacity
-            onPress={() => {
-              onPress({ id: ALL_APPLETS_ID });
-            }}
+          <TabItem
             key="allApplets"
-          >
-            <View style={[
-              styles.tab,
-              this.fireOnInitial || { backgroundColor: colors.lightBlue },
-            ]}
-            >
-              <View style={styles.center}>
-                {renderBorder}
-                <View style={styles.tabImage}>
-                  <AllApplets.AllAppletsIcon size={scaleFactor * size} />
-                </View>
-              </View>
-              <Text numberOfLines={1} ellipsizeMode="tail" style={styles.tabTitle}>All applets</Text>
-            </View>
-          </TouchableOpacity>
+            applet={allAppletsItem}
+            selected={!this.fireOnInitial}
+            onPress={() => {
+              onPress(allAppletsItem);
+            }}
+          />
 
-          {this.renderTabs(data, currentApplet, renderBorder)}
+          {this.renderTabs(data, currentApplet)}
 
         </ScrollView>
       </React.Fragment>
