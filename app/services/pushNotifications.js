5cb0e83349cbac5cbb93220cae811a1ee8dacec2
Eden2016 <eden201621@gmail.com>
2020-01-23 11:18:18 -0500

    Handling with unhandled promise warning for pushNotifications. Adding conditional for ios Platform

--- app/services/pushNotifications.js
+++ app/services/pushNotifications.js
@@ -1,4 +1,4 @@
-import { PushNotificationIOS } from 'react-native';
+import { Platform, PushNotificationIOS } from 'react-native';
 import PushNotification from 'react-native-push-notification';
 // import moment from 'moment';
 
@@ -37,62 +37,64 @@ export const initializePushNotifications = (onNotification) => {
 // }
 
 export const scheduleNotifications = (activities) => {
-  PushNotificationIOS.setApplicationIconBadgeNumber(1);
-  PushNotification.cancelAllLocalNotifications();
-  // const now = moment();
-  // const lookaheadDate = moment().add(1, 'month');
-
   const notifications = [];
 
-  for (let i = 0; i < activities.length; i += 1) {
-    const activity = activities[i];
+  if (Platform.OS === 'ios') {
+    PushNotificationIOS.setApplicationIconBadgeNumber(1);
+    PushNotification.cancelAllLocalNotifications();
+    // const now = moment();
+    // const lookaheadDate = moment().add(1, 'month');
 
-    const scheduleDateTimes = activity.notification || [];
 
-    // /* below is for easy debugging.
-    //    every 30 seconds a notification will appear for an applet.
-    // */
-    // const scheduleDateTimes = [];
+    for (let i = 0; i < activities.length; i += 1) {
+      const activity = activities[i];
 
-    // for (i = 0; i < 50; i += 1) {
-    //   const foo = new Date();
-    //   foo.setSeconds(foo.getSeconds() + i * 30);
-    //   scheduleDateTimes.push(moment(foo));
-    // }
-    // console.log('activity', activity);
-    // /* end easy debugging section */
+      const scheduleDateTimes = activity.notification || [];
 
-    scheduleDateTimes.forEach((dateTime) => {
-      const ugctime = new Date(dateTime.valueOf());
-      notifications.push({
-        timestamp: ugctime,
-        niceTime: dateTime.format(),
-        activityId: activity.id,
-        activityName: activity.name.en,
-        appletName: activity.appletName,
-        activity: JSON.stringify(activity),
+      // /* below is for easy debugging.
+      //    every 30 seconds a notification will appear for an applet.
+      // */
+      // const scheduleDateTimes = [];
+
+      // for (i = 0; i < 50; i += 1) {
+      //   const foo = new Date();
+      //   foo.setSeconds(foo.getSeconds() + i * 30);
+      //   scheduleDateTimes.push(moment(foo));
+      // }
+      // console.log('activity', activity);
+      // /* end easy debugging section */
+
+      scheduleDateTimes.forEach((dateTime) => {
+        const ugctime = new Date(dateTime.valueOf());
+        notifications.push({
+          timestamp: ugctime,
+          niceTime: dateTime.format(),
+          activityId: activity.id,
+          activityName: activity.name.en,
+          appletName: activity.appletName,
+          activity: JSON.stringify(activity),
+        });
+      });
+    }
+    // Sort notifications by timestamp
+    notifications.sort((a, b) => a.timestamp - b.timestamp);
+
+    // Schedule the notifications
+    notifications.forEach((notification) => {
+      PushNotification.localNotificationSchedule({
+        message: `Please perform activity: ${notification.activityName}`,
+        date: new Date(notification.timestamp),
+        group: notification.activityName,
+        vibrate: true,
+        userInfo: {
+          id: notification.activityId,
+          activity: notification.activity,
+        },
+        // android only (notification.activity is already stringified)
+        data: notification.activity,
       });
     });
   }
-
-  // Sort notifications by timestamp
-  notifications.sort((a, b) => a.timestamp - b.timestamp);
-
-  // Schedule the notifications
-  notifications.forEach((notification) => {
-    PushNotification.localNotificationSchedule({
-      message: `Please perform activity: ${notification.activityName}`,
-      date: new Date(notification.timestamp),
-      group: notification.activityName,
-      vibrate: true,
-      userInfo: {
-        id: notification.activityId,
-        activity: notification.activity,
-      },
-      // android only (notification.activity is already stringified)
-      data: notification.activity,
-    });
-  });
 
   return notifications;
 };
