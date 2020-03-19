dfff938af1bcc0252d149e4664f8a0b8b26a363e
ProNinoGit120 <bestmatch19940123@gmail.com>
2020-03-19 06:45:10 -0400

    Update slider styles

--- app/widgets/Slider/index.js
+++ app/widgets/Slider/index.js
@@ -6,15 +6,24 @@ import {
   TouchableWithoutFeedback,
   StyleSheet,
 } from 'react-native';
-import { Text } from 'native-base';
+import { Button, Text } from 'native-base';
 import SliderComponent from 'react-native-slider';
 import { getURL } from '../../services/helper';
-import { colors } from '../../themes/colors';
+
+const testTicks = [
+  { name: 'One', value: 1 },
+  { name: 'Two', value: 2 },
+  { name: 'Three', value: 3 },
+  { name: 'Four', value: 4 },
+  { name: 'Five', value: 5 },
+];
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     paddingTop: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
   },
   sliderWrapper: {
     width: '100%',
@@ -41,7 +50,7 @@ const styles = StyleSheet.create({
     width: 26,
     height: 26,
     borderRadius: 26 / 2,
-    backgroundColor: colors.primary,
+    backgroundColor: '#008060',
     elevation: 4,
     shadowColor: '#000',
     shadowOffset: {
@@ -63,10 +72,53 @@ const styles = StyleSheet.create({
   },
   labelContainer: {
     width: '100%',
+    paddingTop: 35,
     justifyContent: 'space-between',
     flexDirection: 'row',
   },
   labelBox: { width: 100 },
+  plusButton: {
+    position: 'absolute',
+    paddingLeft: 6,
+    paddingRight: 6,
+    paddingTop: 7,
+    paddingBottom: 0,
+    borderRadius: 0,
+    borderColor: '#008060',
+    backgroundColor: '#eee',
+    height: 30,
+    bottom: 7,
+    right: -15,
+  },
+  leftLabel: {
+    fontSize: 25,
+    fontWeight: '800',
+    lineHeight: 25,
+    color: '#008060',
+  },
+  minusButton: {
+    position: 'absolute',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 6,
+    paddingBottom: 0,
+    borderRadius: 0,
+    borderColor: '#008060',
+    backgroundColor: '#eee',
+    height: 30,
+    bottom: 7,
+    left: -15,
+  },
+  rightLabel: {
+    fontSize: 25,
+    fontWeight: '800',
+    lineHeight: 25,
+    color: '#008060',
+  },
+  tickMark: {
+    position: 'absolute',
+    bottom: -34,
+  },
   knobLabel: {
     position: 'absolute',
     bottom: 40,
@@ -86,6 +138,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     fontSize: 12,
     textAlign: 'center',
+  },
+  tickLabel: {
+    paddingLeft: 5,
+    fontSize: 12,
+    color: '#cfcfcf',
   },
 });
 
@@ -115,6 +172,7 @@ class Slider extends Component {
     currentValue: null,
     maximumValue: 7,
     sliderWidth: 0,
+    tickMarks: [],
   };
 
   componentDidMount() {
@@ -134,20 +192,46 @@ class Slider extends Component {
       }),
     );
 
+
     this.setState({ minimumValue: minValue, maximumValue: maxValue });
   }
 
   measureSliderWidth = () => {
+    const { minimumValue, maximumValue } = this.state;
     const { value } = this.props;
-
     this.sliderRef.current.measure((fx, fy, width) => {
-      this.setState({ sliderWidth: width, currentValue: value });
+      const tickMarks = this.getTickMarks(minimumValue, maximumValue, width);
+      this.setState({ sliderWidth: width, currentValue: value, tickMarks });
     });
+  };
+
+  getTickMarks = (start, end, width) => {
+    const tickMark = {
+      value: start,
+      left: this.getTickMark(start, width),
+    };
+    if (start === end) {
+      return [tickMark];
+    }
+    return [tickMark, ...this.getTickMarks(start + 1, end, width)];
+  };
+
+  getTickMark = (value, width) => {
+    const { minimumValue, maximumValue } = this.state;
+    if (value === minimumValue) {
+      return 37;
+    }
+    if (value === maximumValue) {
+      return 14 + width;
+    }
+
+    return (
+      (width - 23) * (value - minimumValue) / (maximumValue - minimumValue) + 37
+    );
   };
 
   handleValue = (value) => {
     const { minimumValue } = this.state;
-
     if (value <= minimumValue) {
       this.setState({ currentValue: minimumValue });
     } else {
@@ -173,8 +257,110 @@ class Slider extends Component {
     this.setState({ currentValue: calculatedValue });
   };
 
-  render() {
+  calculateLabelPosition = () => {
+    const { currentValue, sliderWidth, minimumValue, maximumValue } = this.state;
+    if (!currentValue) {
+      return 22;
+    }
+    if (currentValue === minimumValue) {
+      return 22;
+    }
+    if (currentValue === maximumValue) {
+      return sliderWidth;
+    }
+
+    return (
+      sliderWidth * (currentValue - minimumValue) / (maximumValue - minimumValue)
+      + (22 - 22 * currentValue / maximumValue)
+    );
+  };
+
+  getTickPosition = (value) => {
+    const { sliderWidth } = this.state;
+    const minValue = 1;
+    const maxValue = testTicks.length;
+
+    if (value === minValue) {
+      return {
+        left: 0,
+      };
+    }
+    if (value === maxValue) {
+      return {
+        left: sliderWidth - 5,
+      };
+    }
+    return {
+      left: sliderWidth * (value - minValue) / (maxValue - minValue),
+    };
+  };
+
+  renderTick = (tick, tickWidth) => {
+    const tickStyle = [
+      styles.tick,
+      {
+        width: tickWidth - 20,
+        transform: [{ translateX: -tickWidth / 2 }],
+      },
+      this.getTickPosition(tick.value, tickWidth),
+    ];
+    return (
+      <Text style={tickStyle} key={tick.value}>{tick.name}</Text>
+    );
+  };
+
+  onPressMinus = () => {
     const { minimumValue, maximumValue } = this.state;
+    const { value, onChange } = this.props;
+    let currentVal;
+
+    if (value >= minimumValue) {
+      currentVal = value - 0.25;
+    } else {
+      currentVal = (minimumValue + maximumValue) / 2 + 0.25;
+    }
+
+    if (currentVal < minimumValue) {
+      currentVal = minimumValue;
+    }
+
+    onChange(currentVal);
+    this.setState({ currentValue: currentVal });
+  };
+
+  onPressPlus = () => {
+    const { minimumValue, maximumValue } = this.state;
+    const { value, onChange } = this.props;
+    let currentVal;
+
+    if (value >= minimumValue) {
+      currentVal = value + 0.25;
+    } else {
+      currentVal = (minimumValue + maximumValue) / 2 + 0.25;
+    }
+
+    if (currentVal > maximumValue) {
+      currentVal = maximumValue;
+    }
+
+    onChange(currentVal);
+    this.setState({ currentValue: currentVal });
+  };
+
+  renderTicks() {
+    const { sliderWidth } = this.state;
+    const tickWidth = sliderWidth / testTicks.length;
+    return (
+      <View style={styles.ticks}>
+        {
+          testTicks.map(tick => this.renderTick(tick, tickWidth))
+        }
+      </View>
+    );
+  }
+
+  render() {
+    const { currentValue, minimumValue, maximumValue, tickMarks } = this.state;
 
     const {
       config: { maxValue, minValue, itemList },
@@ -184,27 +370,36 @@ class Slider extends Component {
       onRelease,
     } = this.props;
 
-    let currentValue = value;
-    if (currentValue === minimumValue - 1) {
-      currentValue = minimumValue;
+    let currentVal = value;
+
+    if (currentVal === minimumValue - 1) {
+      currentVal = minimumValue;
     }
 
+    const labelPosition = currentValue ? this.calculateLabelPosition() : 22;
     return (
       <View style={styles.container}>
         <View style={styles.sliderWrapper}>
+          {currentVal !== null && (
+          <View style={[styles.knobLabel, { left: labelPosition }]}>
+            <Text style={styles.knobLabelText}>
+              {currentVal >= minimumValue ? Math.round(currentValue * 100) / 100 : ''}
+            </Text>
+          </View>
+          )}
           <TouchableWithoutFeedback onPressIn={this.tapSliderHandler}>
             <View ref={this.sliderRef} onLayout={this.measureSliderWidth}>
               <SliderComponent
-                value={currentValue >= minimumValue
-                  ? currentValue : (minimumValue + maximumValue) / 2}
+                value={currentVal >= minimumValue
+                  ? currentVal : (minimumValue + maximumValue) / 2}
                 onValueChange={value => this.handleValue(value)}
                 minimumValue={minimumValue}
                 maximumValue={maximumValue}
-                minimumTrackTintColor="#CCC"
+                minimumTrackTintColor={currentVal >= minimumValue ? '#49aF2A' : '#CCC'}
                 maximumTrackTintColor="#CCC"
                 trackStyle={styles.track}
-                thumbStyle={currentValue >= minimumValue ? styles.thumb : styles.thumbUnselected}
-                step={itemList ? 1 : 0}
+                thumbStyle={currentVal >= minimumValue ? styles.thumb : styles.thumbUnselected}
+                step={itemList ? 0.05 : 0}
                 onSlidingStart={onPress}
                 onSlidingComplete={(val) => {
                   onRelease();
