f2434a0e6a851db3d277484b1c719c5c4882b895
ProNinoGit120 <bestmatch19940123@gmail.com>
2020-03-29 21:47:28 -0400

    Slider-bar on NIMH

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ## [0.11.16] - 2019-03-26
+- Updated sliderbar to be discrete for NIMH applet
+
+## [0.11.16] - 2019-03-26
 - Updated activity status automatically every second
 - Updated unit tests for sortActivities
 
--- app/widgets/Slider/index.js
+++ app/widgets/Slider/index.js
@@ -23,8 +23,6 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     paddingTop: 20,
-    paddingRight: 20,
-    paddingLeft: 20,
   },
   sliderWrapper: {
     width: '100%',
@@ -156,6 +154,7 @@ class Slider extends Component {
       maxValue: PropTypes.string,
       itemList: PropTypes.array,
     }).isRequired,
+    appletName: PropTypes.string.isRequired,
     value: PropTypes.number,
     onChange: PropTypes.func.isRequired,
     onPress: PropTypes.func,
@@ -370,11 +369,11 @@ class Slider extends Component {
       onChange,
       onPress,
       value,
+      appletName,
       onRelease,
     } = this.props;
 
     let currentVal = value;
-
     if (currentVal === minimumValue - 1) {
       currentVal = minimumValue;
     }
@@ -390,11 +389,6 @@ class Slider extends Component {
             </Text>
           </View>
           )}
-          <Button onPress={this.onPressPlus} style={styles.plusButton}>
-            <Text style={styles.leftLabel}>
-              +
-            </Text>
-          </Button>
           {tickMarks.map(tickMark => (
             <View key={tickMark.value} style={[styles.tickMark, { left: tickMark.left }]}>
               <Text style={styles.tickLabel}> l </Text>
@@ -413,7 +407,7 @@ class Slider extends Component {
                 maximumTrackTintColor="#CCC"
                 trackStyle={styles.track}
                 thumbStyle={currentVal >= minimumValue ? styles.thumb : styles.thumbUnselected}
-                step={itemList ? 0.05 : 0}
+                step={appletName === 'Healthy Brain Network (NIMH content) v0.24' ? 1 : 0.05}
                 onSlidingStart={onPress}
                 onSlidingComplete={(val) => {
                   onRelease();
@@ -422,11 +416,6 @@ class Slider extends Component {
               />
             </View>
           </TouchableWithoutFeedback>
-          <Button onPress={this.onPressMinus} style={styles.minusButton}>
-            <Text style={styles.leftLabel}>
-              -
-            </Text>
-          </Button>
         </View>
 
         <View style={styles.labelContainer}>
