deee88198aef2fe4ce187e78b4f17019f15ddb6c
Henry Rossiter <henry.rossiter@utexas.edu>
2020-03-31 08:50:48 -0500

    update signupform fields + style

--- app/scenes/Signup/SignupForm.js
+++ app/scenes/Signup/SignupForm.js
@@ -8,12 +8,10 @@ import {
 import { reduxForm, Field, propTypes } from 'redux-form';
 import { FormInputItem, required } from '../../components/form/FormItem';
 import styles from './styles';
+import { colors } from '../../theme';
 
 const login = value => (value && !RegExp('^[a-z][\\da-z\\-\\.]{3,}$').test(value)
   ? 'Username must be at least 4 characters, start with a letter, and may only contain letters, numbers, dashes, and dots.' : undefined);
-
-const email = value => (value && !RegExp('^[\\w\\.\\-\\+]*@[\\w\\.\\-]*\\.\\w+$').test(value)
-  ? 'Invalid email address' : undefined);
 
 const password = value => (value && !RegExp('.{6}.*').test(value)
   ? 'Password must be at least 6 characters' : undefined);
@@ -26,7 +24,7 @@ const SignUpForm = ({ handleSubmit, submitting, primaryColor }) => (
       placeholder="Username"
       name="login"
       style={styles.text}
-      placeholderTextColor="#aaa"
+      placeholderTextColor={colors.secondary_50}
       autoComplete="off"
       autoCorrect={false}
       autoCapitalize="none"
@@ -34,41 +32,20 @@ const SignUpForm = ({ handleSubmit, submitting, primaryColor }) => (
     />
     <Field
       component={FormInputItem}
-      placeholder="First name"
-      name="firstName"
+      placeholder="Display name"
+      name="displayName"
       style={styles.text}
-      placeholderTextColor="#aaa"
+      placeholderTextColor={colors.secondary_50}
       autoComplete="off"
       autoCorrect={false}
       validate={required}
     />
     <Field
       component={FormInputItem}
-      placeholder="Last name"
-      name="lastName"
-      style={styles.text}
-      placeholderTextColor="#aaa"
-      autoComplete="off"
-      autoCorrect={false}
-    />
-    <Field
-      component={FormInputItem}
-      placeholder="Email"
-      name="email"
-      style={styles.text}
-      placeholderTextColor="#aaa"
-      autoComplete="off"
-      autoCorrect={false}
-      autoCapitalize="none"
-      keyboardType="email-address"
-      validate={email}
-    />
-    <Field
-      component={FormInputItem}
       placeholder="Password"
       name="password"
       style={styles.text}
-      placeholderTextColor="#aaa"
+      placeholderTextColor={colors.secondary_50}
       secureTextEntry
       autoComplete="off"
       autoCorrect={false}
@@ -77,14 +54,14 @@ const SignUpForm = ({ handleSubmit, submitting, primaryColor }) => (
     />
     <Button
       warning
-      style={{ marginTop: 40, backgroundColor: primaryColor }}
+      style={styles.button}
       block
       onPress={handleSubmit}
       disabled={submitting}
     >
       {submitting
         ? <ActivityIndicator color={primaryColor} />
-        : <Text style={styles.buttonText}>Sign Up</Text>}
+        : <Text style={[styles.buttonText, { color: primaryColor }]}>Sign Up</Text>}
     </Button>
   </Form>
 );
