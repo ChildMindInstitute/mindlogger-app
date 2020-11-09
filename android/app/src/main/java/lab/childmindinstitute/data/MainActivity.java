4e77dd8ecf399b5f306e959ec1c7f1056a21ae77
jDev <rjtech0018@gmail.com>
2020-11-09 08:08:58 +0200

    disable screenshot on android

--- android/app/src/main/java/lab/childmindinstitute/data/MainActivity.java
+++ android/app/src/main/java/lab/childmindinstitute/data/MainActivity.java
@@ -27,4 +27,13 @@ public class MainActivity extends ReactActivity {
               }
           };
       }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setFlags(
+       WindowManager.LayoutParams.FLAG_SECURE,
+       WindowManager.LayoutParams.FLAG_SECURE
+    );
+  }
 }
