274b513399469e899b416205cf2c156d4024f1e6
binaryExDev <binarydev51@gmail.com>
2020-12-21 21:22:45 -0500

    Fix merge conflicts

--- app/scenes/Activity/ActivityTime.js
+++ app/scenes/Activity/ActivityTime.js
@@ -19,7 +19,8 @@ const styles = StyleSheet.create({
 const ActivityTime = ({ activity, startedTimes, finishActivity }) => {
   let { hour, minute, second, allow } = activity.lastTimedActivity;
   const startedTime = startedTimes ? startedTimes[activity.id] : null;
-  if (startedTime & allow) {
+  
+  if (startedTime && allow) {
     const activityTime = hour * (60000 * 60) + minute * 60000 + second * 1000;
     const difference = Math.abs(Date.now() - startedTime);
     if (activityTime > difference) {
