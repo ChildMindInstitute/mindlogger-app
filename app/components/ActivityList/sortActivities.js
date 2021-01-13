4aa42bef76a5154d8ec1ea18883804e4dd75f4ba
Matchdev120 <bestmatch19940123@gmail.com>
2021-01-12 20:35:17 -0500

    Remove console logs

--- app/components/ActivityList/sortActivities.js
+++ app/components/ActivityList/sortActivities.js
@@ -80,7 +80,6 @@ const addProp = (key, val, arr) => arr.map(obj => R.assoc(key, val, obj));
 // Sort the activities into categories and inject header labels, e.g. "In Progress",
 // before the activities that fit into that category.
 export default (appletId, activityList, inProgress, activityEndTimes) => {
-  console.log('activity list----->', activityList);
   const inProgressKeys = Object.keys(inProgress);
   const inProgressActivities = activityList.filter(activity => inProgressKeys.includes(appletId + activity.id));
 
