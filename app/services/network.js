4f982fc720a552e37496c1eae0d1298bc397ed75
jDev <rjtech0018@gmail.com>
2021-04-10 12:42:00 -0400

    removed unused code

--- app/services/network.js
+++ app/services/network.js
@@ -236,18 +236,6 @@ export const registerOpenApplet = (authToken, schemaURI) => {
   }).then((res) => (res.status === 200 ? res.json() : Promise.reject(res)));
 };
 
-// export const getAppletSchedule = (authToken, appletId) => {
-//   const url = `${apiHost()}/applet/${appletId}/schedule?getTodayEvents=true`;
-//   const headers = {
-//     'Girder-Token': authToken,
-//   };
-//   return fetch(url, {
-//     method: 'get',
-//     mode: 'cors',
-//     headers,
-//   }).then(res => (res.status === 200 ? res.json() : Promise.reject(res)));
-// };
-
 export const getAppletSchedule = (authToken, appletId) => {
   const queryParams = `?${objectToQueryParams({
     getAllEvents: false,
