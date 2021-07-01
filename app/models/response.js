5e305f056d9ffe5b26a96829ff8b2f48370bbea3
devbtech <devtech728@gmail.com>
2021-07-02 01:41:27 +0500

    media upload issue has been solved

--- app/models/response.js
+++ app/models/response.js
@@ -104,7 +104,8 @@ export const prepareResponseForUpload = (
   /** process for encrypting response */
   if (config.encryptResponse && appletMetaData.encryption) {
     const formattedResponses = activity.items.reduce(
-      (accumulator, item, index) => ({ ...accumulator, [item.schema]: (item.inputType == 'drawing' ? responses[index].value : index) }),
+      // (accumulator, item, index) => ({ ...accumulator, [item.schema]: (item.inputType == 'drawing' ? responses[index].value : index) }),
+      (accumulator, item, index) => ({ ...accumulator, [item.schema]: item.inputType == 'drawing' ? responses[index].value : responses[index] ? responses[index].value : index }),
       {},
     );
     const dataSource = getEncryptedData(responses, appletMetaData.AESKey);
