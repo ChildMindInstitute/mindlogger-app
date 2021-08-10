7516d0d370479d4672ca3540548a7f62c4d75a77
binaryExDev <binarydev51@gmail.com>
2021-08-10 07:22:58 -0400

    Update drop-down

--- app/widgets/AgeSelector/index.js
+++ app/widgets/AgeSelector/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Text, Container, ListItem, Left, Right, Icon } from 'native-base';
-import { Picker, Modal, StyleSheet, View } from 'react-native';
+import { Modal, StyleSheet, View } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown'
 import { OptionalText } from '../OptionalText';
 import i18n from 'i18next';
 
@@ -10,9 +11,22 @@ import { colors } from '../../theme';
 const styles = StyleSheet.create({
   paddingContent: {
     flex: 1,
-    borderTopWidth: 2,
-    borderTopColor: colors.grey,
     justifyContent: 'flex-start',
+  },
+  dropDown: {
+    backgroundColor: 'transparent',
+    borderBottomWidth: 1.5,
+    borderBottomColor: colors.lightGrey,
+    color: 'white',
+    
+  },
+  buttonStyle: {
+    fontSize: 13,
+    width: '100%',
+    backgroundColor: 'transparent',
+    borderBottomWidth: 1.5,
+    borderBottomColor: colors.grey,
+    color: colors.grey,
   },
   label: {
     fontSize: 13,
@@ -22,24 +36,11 @@ const styles = StyleSheet.create({
 
 export class AgeSelector extends React.Component {
   finalAnswer = {};
-  state = {
-    modalVisible: false,
-  };
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.value !== this.props.value) {
-      this.setState({ modalVisible: false });
-    }
-  }
 
   handleComment = (itemValue) => {
     const { onChange } = this.props;
     this.finalAnswer["text"] = itemValue;
     onChange(this.finalAnswer);
-  }
-
-  setModalVisible(visible) {
-    this.setState({ modalVisible: visible });
   }
 
   onSelect(v) {
@@ -50,8 +51,6 @@ export class AgeSelector extends React.Component {
   render() {
     const { onChange, value, config, isOptionalText, isOptionalTextRequired } = this.props;
     let itemList = [];
-
-    console.log('value: ', value);
 
     for (let i = config.minAge; i <= config.maxAge; i += 1) {
       itemList.push('' + i);
@@ -67,29 +66,21 @@ export class AgeSelector extends React.Component {
 
     return (
       <View style={{ marginBottom: 0, height: 350 }}>
-        {this.state.modalVisible ? (
           <Container style={styles.paddingContent}>
-            <Picker selectedValue={value?.value} onValueChange={(v) => onChange({ value: v })}>
-
-              {itemList.map((item, index) => (
-                <Picker.Item label={item} value={item} key={index} />
-              ))}
-            </Picker>
-          </Container>
-        ) : (
-            <ListItem
-              onPress={() => {
-                this.setModalVisible(true);
+            <SelectDropdown
+              data={itemList}
+              dropdownStyle={styles.dropDown}
+              buttonStyle={styles.buttonStyle}
+              defaultButtonText={i18n.t('select:select_one')}
+              defaultValue={value ? value.value : i18n.t('select:select_one')}
+              onSelect={(selectedItem) => {
+                onChange({ value: selectedItem })
               }}
-            >
-              <Left>
-                <Text>{value ? value.value : i18n.t('select:select_one')}</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-          )}
+              buttonTextAfterSelection={() => value ? value.value : i18n.t('select:select_one')}
+              rowTextForSelection={(item) => item}
+            />
+          </Container>
+
         {isOptionalText &&
           <OptionalText
             onChangeText={text => this.handleComment(text)}
