0a64ca032c158a04994e3c8f00253c381b49ef57
devbtech <devtech728@gmail.com>
2021-09-30 23:05:23 +0500

    new changes deployed

--- app/widgets/index.js
+++ app/widgets/index.js
@@ -20,6 +20,5 @@ export * from './VisualStimulusResponse';
 export * from './RadioPrizes';
 export * from './TooltipBox';
 export * from './StackedSlider';
-export * from './AgeSelector';
 export * from './StackedRadio';
 export * from './StabilityTracker';
