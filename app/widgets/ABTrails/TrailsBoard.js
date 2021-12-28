986ca7b470892f1fce457e923b0dbe8bb1d6dd2b
jDev <rjtech0018@gmail.com>
2021-12-28 03:28:26 -0500

    send start time

--- app/widgets/ABTrails/TrailsBoard.js
+++ app/widgets/ABTrails/TrailsBoard.js
@@ -46,6 +46,7 @@ export default class TrailsBoard extends Component {
       validIndex: -1,
       currentScreen: 0,
       isValid: false,
+      startTime: 0,
       isStopped: false,
       rate: 1,
     };
@@ -65,7 +66,7 @@ export default class TrailsBoard extends Component {
       onPanResponderRelease: (evt, gestureState) => {
         const { lines, currentIndex } = this.state;
         this.endLine(evt, gestureState);
-        this.props.onResult({ ...this.save(lines, currentIndex) });
+        this.props.onResult({ ...this.save(lines, currentIndex), startTime: this.state.startTime });
       },
     });
     this.allowed = true;
@@ -85,6 +86,8 @@ export default class TrailsBoard extends Component {
       clearInterval(trailsTimerId);
     }
 
+    this.setState({ startTime: new Date().getTime() });
+
     this.timeInterval = setInterval(() => {
       const { currentIndex, failedCnt, currentScreen } = this.props;
       const { lines } = this.state;
@@ -94,9 +97,9 @@ export default class TrailsBoard extends Component {
       if ((currentScreen === 2 && screenTime >= 150) ||
         (currentScreen === 4 && screenTime >= 300)) {
         this.setState({ screenTime: 0 });
-        this.props.onResult({ ...result, screenTime, failedCnt: failedCnt }, true);
+        this.props.onResult({ ...result, screenTime, failedCnt: failedCnt, startTime: this.state.startTime }, true);
       } else {
-        this.props.onResult({ ...result, screenTime, failedCnt: failedCnt });
+        this.props.onResult({ ...result, screenTime, failedCnt: failedCnt, startTime: this.state.startTime });
         this.setState({ screenTime });
       }
     }, 1000)
