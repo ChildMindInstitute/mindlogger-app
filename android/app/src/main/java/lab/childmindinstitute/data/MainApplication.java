341c4007d01e4433e38d90ec8f5a322f74f6f3c8
devbtech <devtech728@gmail.com>
2021-12-31 09:07:11 +0500

    iOS build failed issue is fixed update

--- android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
+++ android/app/src/main/java/lab/childmindinstitute/data/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.ReactNativeBlobUtil.ReactNativeBlobUtilPackage;
 import com.vinzscam.reactnativefileviewer.RNFileViewerPackage;
 import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
