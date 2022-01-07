b9b6610a9bde3a08399e38160765758f8d94c2de
jDev <rjtech0018@gmail.com>
2022-01-07 12:51:09 -0500

    fix app crash

--- app/components/Slider2D.js
+++ app/components/Slider2D.js
@@ -361,18 +361,20 @@ const Slider2D = ({ sliderWidth, padding, onToggle, item, borderRadius, onChange
 
           if (!disabled) {
             updateValues(locationX, locationY, false)
+            onToggle(false);
           }
-          onToggle(false);
         }}
         onResponderMove={(evt) => {
           const { locationX, locationY } = evt.nativeEvent;
           if (!disabled) {
             updateValues(locationX, locationY, false)
+            onToggle(false);
           }
-          onToggle(false);
         }}
         onPanResponderTerminate={() => {
-          onToggle(true);
+          if (!disabled) {
+            onToggle(true);
+          }
         }}
 
         onResponderRelease={(evt) => {
@@ -382,7 +384,7 @@ const Slider2D = ({ sliderWidth, padding, onToggle, item, borderRadius, onChange
             updateValues(locationX, locationY, true)
           }
         }}
-        
+
       ></View>
     </View>
   )
