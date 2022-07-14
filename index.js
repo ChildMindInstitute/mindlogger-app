9c3d5f6162679be9bbe5698b9b68fb0b61ef5446
devbtech <devtech728@gmail.com>
2022-07-13 21:07:22 -0400

    Update shell script, Revert Sentry

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ import setup from './app/setup';
 import * as Sentry from '@sentry/react-native';
 
 
-// if (!__DEV__) {
-//   Sentry.init({
-//     dsn: 'https://9bd31f61c40541cfb0057e85e09b2cda@o414302.ingest.sentry.io/5313182',
-//   });
-// }
+if (!__DEV__) {
+  Sentry.init({
+    dsn: 'https://9bd31f61c40541cfb0057e85e09b2cda@o414302.ingest.sentry.io/5313182',
+  });
+}
 
 AppRegistry.registerComponent('MDCApp', setup);
