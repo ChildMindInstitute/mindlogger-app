88cb283698f150e0743a08276b9b4bba9e8e563b
Nalivaiko, Aleksej <Nalivaiko@scnsoft.com>
2022-12-09 16:49:28 +0300

    Add temp date-time to notification header

--- app/features/notifications/utils.js
+++ app/features/notifications/utils.js
@@ -6,7 +6,7 @@ import { SCENES_TO_NOT_RENDER_NOTIFICATIONS } from './constants';
 export function mapToTriggerNotifications(notifications = []) {
     return notifications.map(notification => ({
         notification: {
-            title: notification.notificationHeader,
+            title: notification.notificationHeader + ": " + new Date().toString(),
             body: notification.notificationBody,
             notificationId: notification.notificationId,
             data: {
